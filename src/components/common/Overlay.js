b8a3272723c8d6dfc8f7e1567a62e052e0fcc18c
Dat Van <vanthanhphuongdat1309@gmail.com>
2022-11-25 01:01:59 +0700

    search results

---
+++ src/components/common/Overlay.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Overlay = () => {
+  return (
+    <div className="fixed inset-0 z-40 invisible bg-black bg-opacity-50 opacity-0"></div>
+  );
+};
+
+export default Overlay;
